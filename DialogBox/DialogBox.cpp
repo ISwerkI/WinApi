d62b5a47258233254d2ae997f83eb10cbc8ed7c1
I_Swerk_I <I_Swerk_I@DESKTOP-1LE6UQ2>
2026-07-06 15:38:07 +1000

    Exit DONE

--- DialogBox/DialogBox.cpp
+++ DialogBox/DialogBox.cpp
@@ -15,6 +15,11 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_INITDIALOG:		
 		break;
 	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK: MessageBox(hwnd, "Была нажата кнопка ОК!", "Info", MB_OK | MB_ICONINFORMATION); break;
+		case IDCANCEL: EndDialog(hwnd, 0);
+		}
 		break;
 	case WM_CLOSE:
 		EndDialog(hwnd, 0);
