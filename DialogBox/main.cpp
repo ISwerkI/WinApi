b1dca08f224a0a30f882b23c2a399f5de32aaff9
I_Swerk_I <I_Swerk_I@DESKTOP-1LE6UQ2>
2026-07-06 16:16:36 +1000

    DialogBox NOT WORK

--- DialogBox/main.cpp
+++ DialogBox/main.cpp
@@ -5,7 +5,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDIGNORE), NULL, (DLGPROC)DlgProc, NULL);
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, NULL);
 };
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -19,6 +19,16 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		{
 		case IDOK: MessageBox(hwnd, "Была нажата кнопка ОК!", "Info", MB_OK | MB_ICONINFORMATION); break;
 		case IDCANCEL: EndDialog(hwnd, 0);
+		case IDC_BUTTON_COPY:
+		{
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+			CONST INT SIZE = 256;
+			CHAR szBuffer[SIZE] = {};
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)szBuffer);
+			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)szBuffer);
+		}
+
 		}
 		break;
 	case WM_CLOSE:
--- DialogBox/resource1.h
+++ DialogBox/resource1.h
@@ -2,11 +2,12 @@
 // Microsoft Visual C++ generated include file.
 // Used by Resource.rc
 //
+#define IDD_DIALOG1                     5
 #define IDC_STATIC_LOGIN                1001
 #define IDC_STATIC_PASSWORD             1002
 #define IDC_EDIT_LOGIN                  1003
 #define IDC_EDIT_PASSWORD               1004
-#define IDC_BUTTON2                     1006
+#define IDBUTTONCOPY                    1006
 #define IDC_BUTTON_COPY                 1006
 
 // Next default values for new objects
