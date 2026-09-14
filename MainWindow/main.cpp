52c58972c797c836f93ee907834f41a8a96fc249
I_Swerk_I <I_Swerk_I@DESKTOP-1LE6UQ2>
2026-09-14 18:46:14 +1000

    Starcraft DONE

--- MainWindow/main.cpp
+++ MainWindow/main.cpp
@@ -36,7 +36,15 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		LR_LOADFROMFILE
 	);
 
-	wClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wClass.hCursor = (HCURSOR)LoadImage
+	(
+		hInstance, 
+		"starcraft-original\\Working In Background.ani",
+		IMAGE_CURSOR,
+		32, 32,
+		LR_LOADFROMFILE
+		);
+	//wClass.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
 
 	wClass.lpszClassName = g_szMyWindowClass;
