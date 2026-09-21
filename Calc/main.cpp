3f4355ff7937f9b3620016fa844f3943448fb7ce
I_Swerk_I <I_Swerk_I@DESKTOP-1LE6UQ2>
2026-09-21 19:03:22 +1000

    Lesson 3 - Digit Buttons DONE

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -1,4 +1,6 @@
 #include <Windows.h>
+#include "resource.h"
+#include "dimentions.h"
 
 CONST CHAR g_szClassName[] = "Calc P_418";
 INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -13,16 +15,16 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	wClass.cbClsExtra = 0;
 	wClass.cbWndExtra = 0;
 
-	wClass.hIcon = (HICON)LoadImage
+	/*wClass.hIcon = (HICON)LoadImage
 	(
 		hInstance, 
 		"ico\\calc_calculator_10824.ico", 
 		IMAGE_ICON, 
 		LR_DEFAULTSIZE,
 		LR_DEFAULTSIZE,
-		LR_LOADFROMFILE);
-	//wClass.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
-	//wClass.hIconSm = LoadIcon(hInstance, IDI_APPLICATION);
+		LR_LOADFROMFILE);*/
+	wClass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
+	wClass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
 	wClass.hCursor = LoadCursor(hInstance, IDC_ARROW);
 	wClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
 
@@ -80,17 +82,52 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			"Edit",
 			"0.",
 			WS_CHILD | WS_VISIBLE | WS_BORDER | ES_RIGHT,
-			10, 10,
-			256, 22,
+			g_i_START_X, g_i_START_Y,
+			g_i_DISPLAY_WIDTH, g_i_DISPLAY_HEIGHT,
 			hwnd,
-			(HMENU)1000,
+			(HMENU)IDC_EDIT_DISPLAY,
 			GetModuleHandle(NULL),	//hInstance
+			NULL
+		);
+		CHAR sz_digit[2] = {};
+		for (int i = 7; i > 0; i -= 3)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				sz_digit[0] = i + j + 48;
+				CreateWindowEx
+				(
+					NULL,
+					"Button",
+					sz_digit,
+					WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+					g_i_BUTTON_START_X + (g_i_BUTTON_SIZE + g_i_INTERVAL)*j,
+					g_i_BUTTON_START_Y + (g_i_BUTTON_SIZE + g_i_INTERVAL)*(2-i/3),
+					g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
+					hwnd,
+					(HMENU)(IDC_BUTTON_0+i+j),
+					GetModuleHandle(NULL),
+					NULL
+				);
+			}
+		}
+		CreateWindowEx
+		(
+			NULL,
+			"Button",
+			"0",
+			WS_CHILD|WS_VISIBLE|BS_PUSHBUTTON,
+			g_i_BUTTON_START_X,g_i_BUTTON_START_Y+(g_i_BUTTON_SIZE+g_i_INTERVAL)*3,
+			g_i_BUTTON_DOUBLE_SIZE,g_i_BUTTON_SIZE,
+			hwnd,
+			(HMENU)IDC_BUTTON_0,
+			GetModuleHandle(NULL),
 			NULL
 		);
 	}
 		break;
 	case WM_COMMAND:
-		break;
+		 break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
