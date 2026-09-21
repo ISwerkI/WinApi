42f1b505e7f9e3daf6970bac289458af008e87bd
I_Swerk_I <I_Swerk_I@DESKTOP-1LE6UQ2>
2026-09-21 19:42:43 +1000

    Lesson 4 - Buttons DONE

--- Calc/dimentions.h
+++ Calc/dimentions.h
@@ -1,11 +1,13 @@
 #pragma once
 
-#define g_i_BUTTON_SIZE		50
-#define g_i_INTERVAL		 2
+#define g_i_BUTTON_SIZE			50
+#define g_i_INTERVAL			2
 #define g_i_BUTTON_DOUBLE_SIZE	g_i_BUTTON_SIZE*2+g_i_INTERVAL
-#define g_i_DISPLAY_WIDTH	300
-#define g_i_DISPLAY_HEIGHT	22
-#define g_i_START_X			10
-#define g_i_START_Y			10
-#define g_i_BUTTON_START_X	g_i_START_X
-#define g_i_BUTTON_START_Y	g_i_START_Y + g_i_DISPLAY_HEIGHT + g_i_INTERVAL
+#define g_i_DISPLAY_WIDTH		256
+#define g_i_DISPLAY_HEIGHT		22
+#define g_i_START_X				10
+#define g_i_START_Y				10
+#define g_i_BUTTON_START_X		g_i_START_X
+#define g_i_BUTTON_START_Y		g_i_START_Y + g_i_DISPLAY_HEIGHT + g_i_INTERVAL	
+#define BUTTON_SHIFT_X(n)		g_i_BUTTON_START_X + (g_i_INTERVAL+g_i_BUTTON_SIZE) * (n)
+#define BUTTON_SHIFT_Y(n)		g_i_BUTTON_START_Y + (g_i_INTERVAL+g_i_BUTTON_SIZE) * (n)
--- Calc/main.cpp
+++ Calc/main.cpp
@@ -3,6 +3,7 @@
 #include "dimentions.h"
 
 CONST CHAR g_szClassName[] = "Calc P_418";
+CONST CHAR* g_OPERATIONS[] = { "+","-","*","/" };
 INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
@@ -101,8 +102,9 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 					"Button",
 					sz_digit,
 					WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-					g_i_BUTTON_START_X + (g_i_BUTTON_SIZE + g_i_INTERVAL)*j,
-					g_i_BUTTON_START_Y + (g_i_BUTTON_SIZE + g_i_INTERVAL)*(2-i/3),
+					//g_i_BUTTON_START_X + (g_i_BUTTON_SIZE + g_i_INTERVAL)*j,
+					//g_i_BUTTON_START_Y + (g_i_BUTTON_SIZE + g_i_INTERVAL)*(2-i/3),
+					BUTTON_SHIFT_X(j),BUTTON_SHIFT_Y(2-i/3),
 					g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
 					hwnd,
 					(HMENU)(IDC_BUTTON_0+i+j),
@@ -124,7 +126,71 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			GetModuleHandle(NULL),
 			NULL
 		);
+		CreateWindowEx
+		(
+			NULL, "Button", ".",
+			WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			BUTTON_SHIFT_X(2), BUTTON_SHIFT_Y(3),
+			g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
+			hwnd,
+			(HMENU)IDC_BUTTON_POINT,
+			GetModuleHandle(NULL),
+			0
+		);
+		for (int i = 0; i < 4; i++)
+		{
+			CreateWindowEx
+			(
+				NULL,"Button",g_OPERATIONS[3-i],
+				WS_CHILD|WS_VISIBLE|BS_PUSHBUTTON,
+				BUTTON_SHIFT_X(3),BUTTON_SHIFT_Y(i),
+				g_i_BUTTON_SIZE,g_i_BUTTON_SIZE,
+				hwnd,
+				(HMENU)(IDC_BUTTON_PLUS + i),
+				GetModuleHandle(NULL),
+				0
+			);
+		}
+
+		CreateWindowEx
+		(
+		NULL,"Button","<-",
+		WS_CHILD|WS_VISIBLE|BS_PUSHBUTTON,
+		BUTTON_SHIFT_X(4),BUTTON_SHIFT_Y(0),
+		g_i_BUTTON_SIZE,g_i_BUTTON_SIZE,
+		hwnd,
+		(HMENU)IDC_BUTTON_BSP,
+		GetModuleHandle(NULL),
+		0
+		);
+
+		CreateWindowEx
+		(
+			NULL,"Button","C",
+			WS_CHILD|WS_VISIBLE|BS_PUSHBUTTON,
+			BUTTON_SHIFT_X(4),BUTTON_SHIFT_Y(1),
+			g_i_BUTTON_SIZE,g_i_BUTTON_SIZE,
+			hwnd,
+			(HMENU)IDC_BUTTON_CLR,
+			GetModuleHandle(0),
+			0
+		);
+
+		CreateWindowEx
+		(
+			NULL,"Button","=",
+			WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			BUTTON_SHIFT_X(4),BUTTON_SHIFT_Y(2),
+			g_i_BUTTON_SIZE,g_i_BUTTON_DOUBLE_SIZE,
+			hwnd,
+			(HMENU)IDC_BUTTON_EQUAL,
+			GetModuleHandle(0),
+			0
+		);
+
 	}
+	
+	
 		break;
 	case WM_COMMAND:
 		 break;
