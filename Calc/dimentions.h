e3601c39782777758e930f45f32075b160bc2324
I_Swerk_I <I_Swerk_I@DESKTOP-1LE6UQ2>
2026-09-21 19:51:00 +1000

    Lesson 4 - TODO: Window Size.

--- Calc/dimentions.h
+++ Calc/dimentions.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#define g_i_BUTTON_SIZE			50
-#define g_i_INTERVAL			2
+#define g_i_BUTTON_SIZE			80
+#define g_i_INTERVAL			20
 #define g_i_BUTTON_DOUBLE_SIZE	g_i_BUTTON_SIZE*2+g_i_INTERVAL
-#define g_i_DISPLAY_WIDTH		256
-#define g_i_DISPLAY_HEIGHT		22
+#define g_i_DISPLAY_WIDTH		g_i_BUTTON_SIZE*5 + g_i_INTERVAL*4
+#define g_i_DISPLAY_HEIGHT		2
 #define g_i_START_X				10
 #define g_i_START_Y				10
 #define g_i_BUTTON_START_X		g_i_START_X
